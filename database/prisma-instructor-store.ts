5eeb909dd9bd78742a48ba93a0a85fc0678b5b20
CHAMUD12 <chamudgamage@gmail.com>
2025-02-04 20:47:53 +0530

    implement Instructor store

--- database/prisma-instructor-store.ts
+++ database/prisma-instructor-store.ts
@@ -1,4 +1,61 @@
-import {PrismaClient} from '@prisma/client';
+// import {PrismaClient} from '@prisma/client';
+// import Instructor from "../model/Instructor";
+//
+// const prisma = new PrismaClient();
+//
+// export async function addInstructor(instructor: Instructor) {
+//     try {
+//         return await prisma.instructor.create({
+//             data: {
+//                 Name: instructor.Name,
+//                 Address: instructor.Address,
+//                 Email: instructor.Email,
+//                 Mobile: instructor.Mobile
+//             }
+//         });
+//     } catch (err) {
+//         console.log("Error adding instructor", err);
+//         throw err;
+//     }
+// }
+//
+// export async function deleteInstructor(id: number) {
+//     try {
+//         return await prisma.instructor.delete({
+//             where: {InstructorId: id}
+//         });
+//     } catch (err) {
+//         console.log("Error deleting instructor", err);
+//         throw err;
+//     }
+// }
+//
+// export async function getAllInstructors() {
+//     try {
+//         return await prisma.instructor.findMany();
+//     } catch (err) {
+//         console.log("Error getting instructors", err);
+//         throw err;
+//     }
+// }
+//
+// export async function updateInstructor(id: number, instructor: Instructor) {
+//     try {
+//         return await prisma.instructor.update({
+//             where: {InstructorId: id},
+//             data: {
+//                 Name: instructor.Name,
+//                 Address: instructor.Address,
+//                 Email: instructor.Email,
+//                 Mobile: instructor.Mobile
+//             }
+//         });
+//     } catch (err) {
+//         console.log("Error updating instructor", err);
+//         throw err;
+//     }
+// }
+import { PrismaClient } from '@prisma/client';
 import Instructor from "../model/Instructor";
 
 const prisma = new PrismaClient();
@@ -10,7 +67,8 @@ export async function addInstructor(instructor: Instructor) {
                 Name: instructor.Name,
                 Address: instructor.Address,
                 Email: instructor.Email,
-                Mobile: instructor.Mobile
+                Mobile: instructor.Mobile,
+                CustomerId: instructor.CustomerId,
             }
         });
     } catch (err) {
@@ -22,7 +80,7 @@ export async function addInstructor(instructor: Instructor) {
 export async function deleteInstructor(id: number) {
     try {
         return await prisma.instructor.delete({
-            where: {InstructorId: id}
+            where: { InstructorId: id }
         });
     } catch (err) {
         console.log("Error deleting instructor", err);
@@ -32,7 +90,11 @@ export async function deleteInstructor(id: number) {
 
 export async function getAllInstructors() {
     try {
-        return await prisma.instructor.findMany();
+        return await prisma.instructor.findMany({
+            include: {
+                Customer: true, // To include customer details
+            }
+        });
     } catch (err) {
         console.log("Error getting instructors", err);
         throw err;
@@ -42,12 +104,13 @@ export async function getAllInstructors() {
 export async function updateInstructor(id: number, instructor: Instructor) {
     try {
         return await prisma.instructor.update({
-            where: {InstructorId: id},
+            where: { InstructorId: id },
             data: {
                 Name: instructor.Name,
                 Address: instructor.Address,
                 Email: instructor.Email,
-                Mobile: instructor.Mobile
+                Mobile: instructor.Mobile,
+                CustomerId: instructor.CustomerId,
             }
         });
     } catch (err) {
